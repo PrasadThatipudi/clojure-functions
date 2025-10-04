609a2b9d4399de2e7573ad2747c8a8d8f5e3e2f8
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-10-04 14:44:52 +0530

    [01]: isPrime works for numbers less than 2

--- 01-prime-checker.clj
+++ 01-prime-checker.clj
@@ -3,5 +3,9 @@
 ;; Implement an efficient algorithm that can handle large numbers (up to 10^6).
 ;; The function should return true if the number is prime, false otherwise.
 ;; Example: 17 -> true, 25 -> false, 2 -> true, 1 -> false
-(def n 17)
+(def number 17)
 ;; START YOUR CODE AFTER THIS LINE. DO NOT REMOVE THIS LINE
+(defn isPrime [number] 
+  (> number 1))
+
+(println (isPrime 1))
