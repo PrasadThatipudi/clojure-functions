dfa33df2bd6a3180485e815c65ead6b675900745
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-10-04 21:00:45 +0530

    [06]: Implements palindromic-number function for all positive numbers

--- 06-palindromic-number.clj
+++ 06-palindromic-number.clj
@@ -3,5 +3,15 @@
 ;; Convert the number to string and check if it's equal to its reverse.
 ;; Do NOT use built-in reverse functions - implement the reversal logic yourself.
 ;; Example: 12321 -> true, 12345 -> false, 7 -> true, 1001 -> true
+(defn div [dividend divisor] (/ (- dividend (mod dividend divisor)) divisor))
 
-(defn palindromic-number? [number])
+(defn reverse-number [number]
+  (loop [reversed-number 0 remaining-number number]
+    (cond (<= remaining-number 0) reversed-number
+          :else (recur
+                 (+ (* reversed-number 10) (mod remaining-number 10))
+                 (div remaining-number 10)))))
+
+(defn palindromic-number? [number] (= (reverse-number number) number))
+
+(print (palindromic-number? 12321))
