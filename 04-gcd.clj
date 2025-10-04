0b29ae7277e8ea594997348afd0aa455604c1135
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-10-04 19:40:05 +0530

    [04]: Implements gcd function

--- 04-gcd.clj
+++ 04-gcd.clj
@@ -4,4 +4,18 @@
 ;; Continue until one number becomes 0, then the other number is the GCD.
 ;; Example: gcd(48, 18) -> 6, gcd(17, 13) -> 1, gcd(100, 25) -> 25
 
-(defn greatest-common-divisor [first-number second-number])
+(defn smaller [first-number second-number]
+  (cond (< first-number second-number) first-number :else second-number))
+
+(defn divisible? [number divisor] (= (mod number divisor) 0))
+
+(defn gcd [first-number second-number]
+  (loop [divisor 1 current-gcd-value 1]
+    (cond  (< (smaller first-number second-number) divisor) current-gcd-value
+           (and
+            (divisible? first-number divisor)
+            (divisible? second-number divisor))
+           (recur (inc divisor) divisor)
+           :else  (recur (inc divisor) current-gcd-value))))
+
+(print (gcd 100 25))
