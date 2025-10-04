4bb603ad521173af4aebdde13cd5de3e177c7a05
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-10-04 19:18:32 +0530

    [03]: Implements perfect-number function

--- 03-perfect-number.clj
+++ 03-perfect-number.clj
@@ -3,5 +3,13 @@
 ;; Proper divisors are all positive divisors except the number itself.
 ;; Find all divisors efficiently and check if their sum equals the original number.
 ;; Example: 28 -> true (divisors: 1,2,4,7,14 and 1+2+4+7+14=28), 12 -> false, 6 -> true
+(defn divisible? [number divisor] (= (mod number divisor) 0))
 
-(defn perfect-number? [number])
+(defn perfect-number? [number]
+  (loop [divisor 1 sum-of-divisors 0]
+    (cond (<= number divisor) (= number sum-of-divisors)
+          (divisible? number divisor)
+          (recur (inc divisor) (+ sum-of-divisors divisor))
+          :else (recur (inc divisor) sum-of-divisors))))
+
+(print (perfect-number? 28))
