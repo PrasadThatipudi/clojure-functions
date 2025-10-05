4daa030c70c088c496c711a217c40d0edf7d232e
PrasadThatipudi <158696031+PrasadThatipudi@users.noreply.github.com>
2025-10-05 13:13:27 +0530

    [04]: Implements gcd function with Euclidean algorithm

--- 04-gcd.clj
+++ 04-gcd.clj
@@ -4,18 +4,11 @@
 ;; Continue until one number becomes 0, then the other number is the GCD.
 ;; Example: gcd(48, 18) -> 6, gcd(17, 13) -> 1, gcd(100, 25) -> 25
 
-(defn smaller [first-number second-number]
-  (cond (< first-number second-number) first-number :else second-number))
-
 (defn divisible? [number divisor] (= (mod number divisor) 0))
 
 (defn gcd [first-number second-number]
-  (loop [divisor 1 current-gcd-value 1]
-    (cond  (< (smaller first-number second-number) divisor) current-gcd-value
-           (and
-            (divisible? first-number divisor)
-            (divisible? second-number divisor))
-           (recur (inc divisor) divisor)
-           :else  (recur (inc divisor) current-gcd-value))))
+  (loop [greatest second-number smallest first-number]
+    (cond  (zero? smallest) greatest
+           :else (recur smallest (mod greatest smallest)))))
 
-(print (gcd 100 25))
+(print (gcd 3 4))
